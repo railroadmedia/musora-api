3cb4a9caa30a28484b218fe9bb1aa6931c17e560
Roxana Riza <roxana_r2000@yahoo.com>
2021-01-15 17:37:54 +0200

    Add package routes, provider and config

---
+++ src/Providers/MusoraApiServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Railroad\MusoraApi\Providers;
+
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+
+class MusoraApiServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        $this->publishes(
+            [
+                __DIR__ . '/../../config/musora-api.php' => config_path('musora-api.php'),
+            ]
+        );
+
+        //load package routes file
+        $this->loadRoutesFrom(__DIR__ . '/../../routes/routes.php');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
