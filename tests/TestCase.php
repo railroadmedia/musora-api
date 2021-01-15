a5212cf336035bb2b19a3c8632c0ee3e85cf3a1c
Roxana Riza <roxana_r2000@yahoo.com>
2021-01-15 15:10:00 +0200

    Default structure

---
+++ tests/TestCase.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Railroad\MobileEndpoints\Tests;
+
+use Orchestra\Testbench\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+
+    }
+}
---
+++ tests/UnitTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use Railroad\MobileEndpoints\Tests\TestCase;
+
+class UnitTest extends TestCase
+{
+    public function test_that_true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
