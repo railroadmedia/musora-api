cdef3168646335982348a555eb803435b3428c51
Roxana Riza <roxana_r2000@yahoo.com>
2021-01-18 17:53:37 +0200

    Login based on jwt token

---
+++ src/Middleware/MobileAppTokenAuth.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Railroad\MusoraApi\Middleware;
+
+use Closure;
+use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
+use Tymon\JWTAuth\JWTAuth;
+
+class MobileAppTokenAuth extends BaseMiddleware
+{
+    /**
+     * TokenAuth constructor.
+     *
+     * @param JWTAuth $auth
+     */
+    public function __construct(JWTAuth $auth)
+    {
+        parent::__construct($auth);
+    }
+
+    /**
+     * @param $request
+     * @param Closure $next
+     * @return mixed
+     * @throws JWTException
+     */
+    public function handle($request, Closure $next)
+    {
+        try {
+            
+            $user =
+                $this->auth->parseToken()
+                    ->getPayload()
+                    ->get('sub');
+
+            auth()->loginUsingId($user, false);
+
+            return $next($request);
+
+        } catch (\Exception $exception) {
+
+            $exceptionInstance = get_class($exception);
+
+            switch ($exceptionInstance) {
+                case \Tymon\JWTAuth\Exceptions\TokenExpiredException::class:
+                    return response()->json(['error' => 'TOKEN_EXPIRED']);
+                case \Tymon\JWTAuth\Exceptions\TokenInvalidException::class:
+                    return response()->json(['error' => 'TOKEN_INVALID']);
+                case \Tymon\JWTAuth\Exceptions\TokenBlacklistedException::class:
+                    return response()->json(['error' => 'TOKEN_BLACKLISTED']);
+                case \Tymon\JWTAuth\Exceptions\JWTException::class:
+                    error_log(' ----------------------------------------------------------     Mobile App Token Exception :::::: '.$exception->getMessage());
+                    return response()->json(['error' => 'Token not provided']);
+                default:
+                    return response()->json(['error' => $exception->getMessage()]);
+            }
+        }
+
+    }
+}
