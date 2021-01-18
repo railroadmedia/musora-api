29cc7183cced4d360fcb1b8fc930d7f473b23aec
Roxana Riza <roxana_r2000@yahoo.com>
2021-01-18 17:53:59 +0200

    Pull content endpoint

--- routes/routes.php
+++ routes/routes.php
@@ -1,13 +1,17 @@
 <?php
 
+use Railroad\MusoraApi\Controllers\ContentController;
+
 Route::group(
     [
         'prefix' => 'musora-api',
+        'middleware' => config('musora-api.middleware'),
     ],
     function () {
 
         Route::get(
             '/content/{id}',
-            \Railroad\MusoraApi\Controllers\ContentController::class . '@getContent'
+            ContentController::class . '@getContent'
         );
-            });
+    }
+);
--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -1,11 +1,54 @@
 <?php
+
 namespace Railroad\MusoraApi\Controllers;
 
 use Illuminate\Routing\Controller;
+use Railroad\Railcontent\Repositories\CommentRepository;
+use Railroad\Railcontent\Services\CommentService;
+use Railroad\Railcontent\Services\ContentService;
+use Railroad\Railcontent\Support\Collection;
 
 class ContentController extends Controller
 {
-    public function getContent(\Illuminate\Http\Request $request){
-       return response()->json('test');
+
+    /**
+     * @var ContentService
+     */
+    private $contentService;
+
+    /**
+     * @var CommentService
+     */
+    private $commentService;
+
+    /**
+     * @var VimeoVi
+     */
+    private $vimeoVideoSourcesDecorator;
+
+
+
+    /**
+     * @param $contentId
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function getContent($contentId, \Illuminate\Http\Request $request)
+    {
+        $content = $this->contentService->getById($contentId);
+
+        if(!$content){
+            return response()->json();
+        }
+
+        $isDownload = $request->get('download', false);
+
+        CommentRepository::$availableContentId = $content['id'];
+        $comments = $this->commentService->getComments(1, 10, '-created_on');
+        $content['comments'] = $comments['results'];
+        $content['total_comments'] = $comments['total_results'];
+
+        return response()->json(['data' => [$content]]);
     }
 }
