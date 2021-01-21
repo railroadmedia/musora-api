4b59c3d9f13d2f9fce68d0d780c5702ff454adee
Roxana Riza <roxana_r2000@yahoo.com>
2021-01-21 18:32:49 +0200

    Define new routes for musora-api endpoints to avoid issue with the prod mobile app that will still continue to use the old endpoints before Apple approval

---
+++ src/Decorators/MobileAppUrlDecorator.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Railroad\MusoraApi\Decorators;
+
+use Railroad\Railcontent\Entities\CommentEntity;
+use Railroad\Railcontent\Entities\ContentEntity;
+use Railroad\Railcontent\Support\Collection;
+
+class MobileAppUrlDecorator extends ModeDecoratorBase
+{
+    public function decorate(Collection $contents)
+    {
+        foreach ($contents as $content) {
+            if ($content['type'] == 'pack' ||
+                $content['type'] == 'semester-pack' ||
+                $content['type'] == 'pack-bundle') {
+                $content['mobile_app_url'] = url()->route('mobile.pack.show', [$content['id']]);
+            } elseif ($content['type'] == 'pack-bundle-lesson') {
+                $content['mobile_app_url'] = url()->route('mobile.pack.lesson.show', [$content['id']]);
+            }
+        }
+
+        return $contents;
+    }
+}
