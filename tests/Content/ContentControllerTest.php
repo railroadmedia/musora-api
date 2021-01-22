a336eb49d8e9f0e5b6191b4e8b42480a697397ab
Roxana Riza <roxana_r2000@yahoo.com>
2021-01-22 16:50:41 +0200

    Prepare tests config

--- tests/Content/ContentControllerTest.php
+++ tests/Content/ContentControllerTest.php
@@ -2,21 +2,36 @@
 
 namespace Railroad\MusoraApi\Tests\Content;
 
-
-
-use Railroad\MusoraAPi\Tests\TestCase;
+use Railroad\MusoraApi\Tests\TestCase;
+use Railroad\Railcontent\Factories\ContentFactory;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class ContentControllerTest extends TestCase
 {
+    /**
+     * @var ContentFactory
+     */
+    protected $contentFactory;
 
     protected function setUp()
     {
         parent::setUp();
+
+        //$this->contentFactory = $this->app->make(ContentFactory::class);
     }
 
     public function test_first()
     {
       $this->assertTrue(true);
+    }
+
+    public function test_pull_content_endpoint()
+    {
+        $response = $this->actingAs()->get(
+             '/musora-api/content/1'
+        );
+
+        $response->assertStatus(200);
     }
 
 
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,15 +1,30 @@
 <?php
 
-namespace Railroad\MusoraAPi\Tests;
+namespace Railroad\MusoraApi\Tests;
 
 use Orchestra\Testbench\TestCase as BaseTestCase;
+use Railroad\MusoraApi\Middleware\MobileAppTokenAuth;
 use Railroad\MusoraApi\Providers\MusoraApiServiceProvider;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Http\Middleware\Authenticate;
 
 class TestCase extends BaseTestCase
 {
     protected function setUp()
     {
         parent::setUp();
+
+        error_reporting(E_ALL);
+
+        $this->artisan('cache:clear', []);
+
+        //call the MobileAppTokenAuth
+//        $middleware = $this->app->make(MobileAppTokenAuth::class);
+//        $middleware->handle(
+//            request(),
+//            function () {
+//            }
+//        );
     }
 
     /**
@@ -20,6 +35,70 @@ class TestCase extends BaseTestCase
      */
     protected function getEnvironmentSetUp($app)
     {
+
+        $defaultConfig = require(__DIR__ . '/../config/railcontent.php');
+
+        $app['config']->set('railcontent.database_connection_name','testbench');
+        $app['config']->set('railcontent.cache_duration', $defaultConfig['cache_duration']);
+        $app['config']->set('railcontent.table_prefix', $defaultConfig['table_prefix']);
+        $app['config']->set('railcontent.data_mode', $defaultConfig['data_mode']);
+        $app['config']->set('railcontent.brand', $defaultConfig['brand']);
+        $app['config']->set('railcontent.available_brands', $defaultConfig['available_brands']);
+        $app['config']->set('railcontent.available_languages', $defaultConfig['available_languages']);
+        $app['config']->set('railcontent.default_language', $defaultConfig['default_language']);
+        $app['config']->set('railcontent.field_option_list', $defaultConfig['field_option_list']);
+        $app['config']->set('railcontent.commentable_content_types', $defaultConfig['commentable_content_types']);
+        $app['config']->set('railcontent.showTypes', $defaultConfig['showTypes']);
+        $app['config']->set('railcontent.cataloguesMetadata', $defaultConfig['cataloguesMetadata']);
+        $app['config']->set('railcontent.topLevelContentTypes', $defaultConfig['topLevelContentTypes']);
+        $app['config']->set('railcontent.userListContentTypes', $defaultConfig['userListContentTypes']);
+        $app['config']->set('railcontent.appUserListContentTypes', $defaultConfig['appUserListContentTypes']);
+        $app['config']->set('railcontent.onboardingContentIds', $defaultConfig['onboardingContentIds']);
+        $app['config']->set('railcontent.validation', $defaultConfig['validation']);
+        $app['config']->set(
+            'railcontent.comment_assignation_owner_ids',
+            $defaultConfig['comment_assignation_owner_ids']
+        );
+        $app['config']->set('railcontent.searchable_content_types', $defaultConfig['searchable_content_types']);
+        $app['config']->set('railcontent.statistics_content_types', $defaultConfig['statistics_content_types']);
+        $app['config']->set('railcontent.search_index_values', $defaultConfig['search_index_values']);
+        $app['config']->set(
+            'railcontent.allowed_types_for_bubble_progress',
+            $defaultConfig['allowed_types_for_bubble_progress']
+        );
+        $app['config']->set('railcontent.all_routes_middleware', $defaultConfig['all_routes_middleware']);
+        $app['config']->set('railcontent.user_routes_middleware', $defaultConfig['user_routes_middleware']);
+        $app['config']->set(
+            'railcontent.administrator_routes_middleware',
+            $defaultConfig['administrator_routes_middleware']
+        );
+
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set(
+            'database.connections.testbench',
+            [
+                'driver' => 'sqlite',
+                'database' => ':memory:',
+                'prefix' => '',
+            ]
+        );
+
         $app->register(MusoraApiServiceProvider::class);
+    }
+
+    /**
+     * Set the currently logged in user for the application.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable $user
+     * @param  string|null                                $driver
+     * @return $this
+     */
+    public function actingAs($user, $driver = null)
+    {
+        $token = JWTAuth::fromUser($user);
+        $this->withHeader('Authorization', "Bearer {$token}");
+        parent::actingAs($user);
+
+        return $this;
     }
 }
