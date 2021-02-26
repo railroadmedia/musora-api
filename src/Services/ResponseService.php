9033ac2c6abfe12220778f5108026fdb3920aab7
Roxana Riza <roxana_r2000@yahoo.com>
2021-02-26 17:16:08 +0200

    Optimization: Replace Fractal with json response

--- src/Services/ResponseService.php
+++ src/Services/ResponseService.php
@@ -13,6 +13,7 @@ use Railroad\MusoraApi\Transformers\FilterOptionsTransformer;
 use Railroad\MusoraApi\Transformers\LiveTransformer;
 use Railroad\MusoraApi\Transformers\PacksTransformer;
 use Railroad\MusoraApi\Transformers\ScheduledContentTransformer;
+use Railroad\Railcontent\Entities\ContentFilterResultsEntity;
 use Spatie\Fractal\Fractal;
 
 class ResponseService
@@ -29,18 +30,32 @@ class ResponseService
 
     public static function catalogue($data, $request)
     {
-        return Fractal::create()
-            ->collection($data->results())
-            ->transformWith(CatalogueTransformer::class)
-            ->addMeta(
-                [
-                    'totalResults' => $data->totalResults(),
-                    'page' => $request->get('page', 1),
-                    'limit' => $request->get('limit', 10),
-                    'filterOptions' => (new FilterOptionsTransformer())->transform($data->filterOptions()),
-                ]
-            )
-            ->toArray();
+        $transformer = new CatalogueTransformer();
+        $transformedData = $transformer->transform($data->results());
+
+        $filters = $data->filterOptions();
+        $filters['showSkillLevel'] = true;
+
+        if ($request->get('included_types', []) == ['coach-stream']) {
+            $filters = ['content_type' => ['coach-stream']];
+        }
+
+        foreach ($filters as $key => $filterOptions) {
+            if (is_array($filterOptions)) {
+                if (($key != 'content_type') && ($key != 'instructor')) {
+                    $filters[$key] = array_diff($filterOptions, ['All']);
+                    array_unshift($filters[$key], 'All');
+                }
+            }
+        }
+
+        return (new ContentFilterResultsEntity(
+            [
+                'results' => $transformedData,
+                'filter_options' => $filters,
+                'total_results' => $data->totalResults(),
+            ]
+        ))->toJsonResponse();
     }
 
     public static function content($data)
--- src/Transformers/CatalogueTransformer.php
+++ src/Transformers/CatalogueTransformer.php
@@ -6,29 +6,31 @@ use Railroad\Railcontent\Helpers\ContentHelper;
 
 class CatalogueTransformer extends \League\Fractal\TransformerAbstract
 {
-    public function transform($content)
+    public function transform($contents)
     {
-        if(!is_array($content)){
-            $content = $content->getArrayCopy();
-        }
-
-        $responseStructure = config('musora-api.response-structure.catalogues', []);
-        if(!$responseStructure){
-           return $content;
-        }
-
         $response = [];
 
-        foreach ($responseStructure as $item) {
-            $fields = explode('.', $item);
-            if (count($fields) == 2) {
-                if($fields[0] == 'data') {
-                    $response[$fields[1]] = (ContentHelper::getDatumValue($content, $fields[1]));
-                }elseif($fields[0] == 'fields'){
-                    $response[$fields[1]] = (ContentHelper::getFieldValue($content, $fields[1]));
+        $responseStructure = config('musora-api.response-structure.catalogues', []);
+        if (!$responseStructure) {
+            return $contents;
+        }
+
+        foreach($contents as $index=>$content) {
+            if (!is_array($content)) {
+                $content = $content->getArrayCopy();
+            }
+
+           foreach ($responseStructure as $item) {
+                $fields = explode('.', $item);
+                if (count($fields) == 2) {
+                    if ($fields[0] == 'data') {
+                        $response[$index][$fields[1]] = (ContentHelper::getDatumValue($content, $fields[1]));
+                    } elseif ($fields[0] == 'fields') {
+                        $response[$index][$fields[1]] = (ContentHelper::getFieldValue($content, $fields[1]));
+                    }
+                } else {
+                    $response[$index][$item] = $content[$item] ?? false;
                 }
-            } else {
-                $response[$item] = $content[$item] ?? false;
             }
         }
 
