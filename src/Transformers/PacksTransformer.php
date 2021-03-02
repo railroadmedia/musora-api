ba494b780e8714db85f0f0f5a84f478fe7726afb
Roxana Riza <roxana_r2000@yahoo.com>
2021-03-02 16:39:03 +0200

    Fix packs response structure

--- src/Transformers/PacksTransformer.php
+++ src/Transformers/PacksTransformer.php
@@ -18,17 +18,18 @@ class PacksTransformer extends TransformerAbstract
         $myPacksResponseStructure = config('musora-api.response-structure.my-packs');
         $morePacksResponseStructure = config('musora-api.response-structure.more-packs');
 
-        if (!empty($topPackResponseStructure)) {
+        if (empty($topPackResponseStructure)) {
             $top = $packs['topHeaderPack'];
         }
 
-        if (!empty($myPacksResponseStructure)) {
+        if (empty($myPacksResponseStructure)) {
             $myPacks = $packs['myPacks'];
         }
 
-        if (!empty($morePacksResponseStructure)) {
+        if (empty($morePacksResponseStructure)) {
             $morePacks = $packs['morePacks'];
         }
+
         if ($packs['topHeaderPack']) {
             foreach ($topPackResponseStructure ?? [] as $key => $item) {
                 if (is_array($item)) {
