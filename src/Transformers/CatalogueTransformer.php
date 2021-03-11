6b6f1da33f16d6b69333c6a8ab30d3713ff5169b
Roxana Riza <roxana_r2000@yahoo.com>
2021-03-11 12:29:16 +0200

    Refactoring

--- src/Transformers/CatalogueTransformer.php
+++ src/Transformers/CatalogueTransformer.php
@@ -2,12 +2,9 @@
 
 namespace Railroad\MusoraApi\Transformers;
 
-use League\Fractal\TransformerAbstract;
-use Railroad\Railcontent\Helpers\ContentHelper;
-
-class CatalogueTransformer extends TransformerAbstract
+class CatalogueTransformer extends ContentTransformer
 {
-    public function transform($contents)
+    public function transform($contents, $responseStructure = [])
     {
         $response = [];
 
@@ -16,23 +13,8 @@ class CatalogueTransformer extends TransformerAbstract
             return $contents;
         }
 
-        foreach ($contents as $index => $content) {
-            if (!is_array($content)) {
-                $content = $content->getArrayCopy();
-            }
-
-            foreach ($responseStructure as $item) {
-                $fields = explode('.', $item);
-                if (count($fields) == 2) {
-                    if ($fields[0] == 'data') {
-                        $response[$index][$fields[1]] = (ContentHelper::getDatumValue($content, $fields[1]));
-                    } elseif ($fields[0] == 'fields') {
-                        $response[$index][$fields[1]] = (ContentHelper::getFieldValue($content, $fields[1]));
-                    }
-                } elseif (array_key_exists($item, $content)) {
-                    $response[$index][$item] = $content[$item];
-                }
-            }
+        foreach ($contents as $content) {
+            $response[] = parent::transform($content, $responseStructure);
         }
 
         return $response;
