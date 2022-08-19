fd7bc955de7859c2c35a6a669d834a07cf1f2022
Roxana Riza <roxana_r2000@yahoo.com>
2022-08-19 15:25:40 +0300

    Add versioning system on live event endpoint

--- src/Transformers/LiveTransformer.php
+++ src/Transformers/LiveTransformer.php
@@ -6,7 +6,12 @@ class LiveTransformer extends ContentTransformer
 {
     public function transform($content, $responseStructure = [])
     {
+        $apiVersion = (config('musora-api.api.version'));
         $responseStructure = config('musora-api.response-structure.live', []);
+        if ($apiVersion) {
+            $responseStructure = config('response.'.$apiVersion.'.live', []);
+        }
+
         if (!$responseStructure) {
             return $content->getArrayCopy();
         }
