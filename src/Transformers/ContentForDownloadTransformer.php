49e56ac5a0c47e59ae00b3449b9d21fad51abd79
Roxana Riza <roxana_r2000@yahoo.com>
2022-08-26 16:05:42 +0300

    Updated downloaded content transformer to support versioning and the new response structure for unified app

--- src/Transformers/ContentForDownloadTransformer.php
+++ src/Transformers/ContentForDownloadTransformer.php
@@ -8,11 +8,17 @@ class ContentForDownloadTransformer extends ContentTransformer
     {
         if (isset( $content['type'])) {
             $type = (in_array($content['type'], config('railcontent.showTypes')[config('railcontent.brand')] ?? [])) ? 'show-lesson' : $content['type'];
-
+            $apiVersion = (config('musora-api.api.version'));
             $responseStructure = array_merge(
                 config('musora-api.response-structure.' . $type,[]),
                 config('musora-api.response-structure.download',[])
             );
+            if ($apiVersion) {
+                $responseStructure =array_merge(
+                    config('response.'.$apiVersion.'.'.$type, []),
+                    config('response.'.$apiVersion.'.download', [])
+                );
+            }
         }
 
        return parent::transform($content, $responseStructure);
