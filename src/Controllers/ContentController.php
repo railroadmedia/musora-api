a901b121ef35aa97d97d99f106548ad7049eb5b0
Roxana Riza <roxana_r2000@yahoo.com>
2023-03-27 16:13:33 +0300

    Fix related playlists

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -2199,7 +2199,7 @@ class ContentController extends Controller
 
         $userPlaylists = collect($this->userPlaylistsService->getUserPlaylist(user()->id, 'user-playlist'));
         $playlists =
-            $userPlaylists->whereNotIn('id', $playlistContent['user_playlist_id']);
+            $userPlaylists->whereNotIn('id', $playlistContent['user_playlist_id'])->values();
         $content['related_playlists'] = $playlists;
 
         ContentRepository::$pullFutureContent = $oldFutureContent;
