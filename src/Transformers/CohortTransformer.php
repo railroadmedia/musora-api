f4660e52ddf6da21cec49570c8f90791e5c36d19
Roxana Riza <roxana_r2000@yahoo.com>
2023-04-20 13:47:42 +0300

    Add formatted data on cohort template

--- src/Transformers/CohortTransformer.php
+++ src/Transformers/CohortTransformer.php
@@ -24,6 +24,16 @@ class CohortTransformer extends TransformerAbstract
             ->format('Y/m/d H:i:s');
         $content['cohort']['enrollment_end_date'] =  Carbon::parse($content['cohort']['enrollment_end_date'])
             ->format('Y/m/d H:i:s');
+
+        $content['cohort']['cohort_start_date_fomatted'] =  Carbon::parse($content['cohort']['cohort_start_date'])
+            ->format('F jS');
+        $content['cohort']['cohort_end_date_fomatted'] =  Carbon::parse($content['cohort']['cohort_end_date'])
+            ->format('F jS');
+        $content['cohort']['enrollment_start_date_fomatted'] =  Carbon::parse($content['cohort']['enrollment_start_date'])
+            ->format('F jS');
+        $content['cohort']['enrollment_end_date_fomatted'] =  Carbon::parse($content['cohort']['enrollment_end_date'])
+            ->format('F jS');
+
         if (!$responseStructure) {
             return $content;
         }
